4f92ab5df4276b18ff14f1bbd5144a5e12ae203d
ippolyte <33734987+ippolyte@users.noreply.github.com>
2019-09-05 15:58:50 -0400

    Add files via upload

---
+++ Demo1.py
@@ -0,0 +1,18 @@
+# This is my first IDCE 302 code
+# Created: September 5th, 2019
+# Name: Aggeliki Barberopoulou
+
+
+# Define variables
+
+pi=3.14
+r=2.0
+area=pi*(r**2)
+perim=2.0*pi*r
+
+#Print results
+print "Given r:", r
+print "Area is:", area
+print "perimeter is:", perim
+
+
