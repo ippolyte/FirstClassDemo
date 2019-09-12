0fd33a852dbb1fc22123e2502a364fcab651565e
ippolyte <33734987+ippolyte@users.noreply.github.com>
2019-09-12 15:48:45 -0400

    Add files via upload

---
+++ Chapter3_example1.py
@@ -0,0 +1,10 @@
+def print_lyrics():
+    print ("I am a lumberjack, and I am okay.")
+    print ("I sleep all night and I work all day.")
+
+    def repeat_lyrics():
+        print_lyrics()
+        
+    
+        repeat_lyrics()
+        print_lyrics()
