2fc17488dc593f529d483faf9051ddae91641ee4
ippolyte <33734987+ippolyte@users.noreply.github.com>
2019-09-12 15:49:22 -0400

    Add files via upload

---
+++ ifequal.py
@@ -0,0 +1,15 @@
+# In Class practice example
+# IDCE Intro to Python Programming
+# Created by Aggeliki Barberopoulou
+# on September 12th 2019
+
+def ifequal(first,second):
+    if first>second:
+       print (first, " is greater than", second)
+    elif(first<second):
+       print (second,"is greater than", first)
+    else:
+       print ("numbers are equal")
+
+          
+    
